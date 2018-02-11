1600bd06499289c79f1cd82d3bed0ea11697215f
Jim Simon <jim.j.simon@gmail.com>
2018-02-11 17:01:44 -0500

    Add globals to the console

--- packages/mvc/console.js
+++ packages/mvc/console.js
@@ -1,4 +1,5 @@
 const repl = require("repl")
+const globalManager = require('./utils/global-manager-singleton')
 
 // const {createNamespace} = require('cls-hooked')
 // const shardingNamespace = createNamespace('sharding')
@@ -8,6 +9,7 @@ const repl = require("repl")
   const replServer = repl.start({
     prompt: 'mvc> '
   })
+  Object.assign(replServer.context, globalManager.context)
   // Object.assign(replServer.context, models, {shardingNamespace})
 // })
 
